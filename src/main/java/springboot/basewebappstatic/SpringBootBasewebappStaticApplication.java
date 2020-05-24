b8c18686aa29c449e2ac0bb35ab767d7579840c5
Banee Ishaque K <Baneeishaque@gmail.com>
2020-05-24 20:10:04 +0530

    Initial commit

---
+++ src/main/java/springboot/basewebappstatic/SpringBootBasewebappStaticApplication.java
@@ -0,0 +1,13 @@
+package springboot.basewebappstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasewebappStaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasewebappStaticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/basewebappstatic/SpringBootBasewebappStaticApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.basewebappstatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasewebappStaticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
